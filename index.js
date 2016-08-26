8e9b993c1962872a837046b7d9745f620c1f6bf0
Kilian Koeltzsch <me@kilian.io>
2016-08-26 11:02:28 +0200

    use merchant or partner name

--- index.js
+++ index.js
@@ -8,15 +8,26 @@ const auth = JSON.parse(fs.readFileSync('./auth.json', 'utf8'))
 const myAccount = new N26(auth.username, auth.password)
   .then(account => account.transactions() // {text: 'searchstring'}
   .then(transactions => {
-    transactions = transactions.slice(0,6)
-    let items = transactions.map(trans => {
-      return {
-        'title': `${trans.amount}€ ${trans.merchantName}`,
-        'subtitle': `${trans.createdTS}`
-      }
-    })
+    let items = transactions.map(createItem)
     console.log(JSON.stringify(toAlfred(items)))
   }))
+
+function createItem(transaction) {
+  let name = ''
+  if (transaction.merchantName !== undefined) name = transaction.merchantName
+  else if (transaction.partnerName !== undefined) name = transaction.partnerName
+
+  return {
+    'title': `${transaction.amount}${substituteCurrencySymbol(transaction.currencyCode)} ${name}`,
+    'subtitle': transaction.createdTS
+  }
+}
+
+function substituteCurrencySymbol(identifier) {
+  if (identifier === "EUR") return "€"
+  if (identifier === "USD") return "$"
+  else return identifier
+}
 
 function toAlfred(items) {
   return {
